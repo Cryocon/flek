561df3bc14f55b7b176a376abc8728f15a63a1ce
jamespalmer <jamespalmer>
2000-02-03 07:37:43 +0000

    Initial check in of Fl_Gl_Arcball_Window.

---
+++ src/Fl_Gl_Arcball_Window.cxx
@@ -0,0 +1,37 @@
+#include <FL/Fl.H>
+#include <Flek/Fl_Gl_Arcball_Window.H>
+
+int Fl_Gl_Arcball_Window::handle (int event)
+{
+  switch (event)
+    {
+     case FL_PUSH:
+      arcball.beginDrag ();
+      break;
+     case FL_RELEASE:
+      arcball.endDrag ();
+      break;
+     case FL_DRAG:
+	{
+	  drag.set ((2.0*Fl::event_x ()) / w() - 1.0, (-2.0 * Fl::event_y ()) / h() + 1.0, 0);
+	  arcball.mouse (drag);
+	  arcball.update ();
+	  redraw ();
+	}
+      break;
+     default:
+      return Fl_Gl_Window::handle (event);
+    }
+  return 1;
+}
+
+void Fl_Gl_Arcball_Window::arcball_draw ()
+{
+  arcball.draw ();  
+}
+
+void Fl_Gl_Arcball_Window::arcball_transform ()
+{
+  glMultMatrixd ((arcball.value ()).value ());
+}
+
