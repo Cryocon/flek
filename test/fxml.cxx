10918042481cdc7191ae5a91c207e6c14f70c4bc
jamespalmer <jamespalmer>
2000-04-07 19:11:24 +0000

    Added xml test apps.

---
+++ test/fxml.cxx
@@ -0,0 +1,32 @@
+#include <Flek/fXml.h>
+
+void main ()
+{
+  fXmlDocument doc = fXmlDocument::parseFile ("fxml.xml");
+  fXmlNode node = doc.children ();
+
+  if (doc.valid ())
+    if (doc.name ())
+      printf ("doc name=%s\n", doc.name ());
+
+  node = node.children ();
+
+  while (node.valid ())
+    {
+      if (!strcmp (node.name (), "transaction"))
+	{
+	  printf ("node name=%s\n", node.name ());
+	  fXmlAttr attribute = node.properties ();
+	  
+	  while (attribute.valid ())
+	    {
+	      fXmlNode property = attribute.value ();
+	      if (property.valid ())
+		printf ("property=%s\n", property.content ());
+	      attribute = attribute.next ();
+	    }
+	}
+      node = node.next ();
+    }
+
+}
