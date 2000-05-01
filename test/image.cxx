bc8eea629a73a0f2b300991988ce800f73688d7c
jamespalmer <jamespalmer>
2000-05-01 04:11:05 +0000

    Mostly new FImage features.  Multi-channel, flip operation, etc.

--- test/image.cxx
+++ test/image.cxx
@@ -1,6 +1,7 @@
 #include <Flek/FImage.H>
 #include <Flek/FPNM.H>
 #include <Flek/FSGI.H>
+#include <Flek/FJPEG.H>
 #include <stdio.h>
 
 #define TEST
@@ -9,8 +10,9 @@ void main ()
 {
   FImage *imgA, *imgB; 
 
-  imgA = FPNM::read ("./test1.ppm");
-  imgB = FSGI::read ("./test2.rgb");
+  //imgA = FPNM::read ("./test1.ppm");
+  //imgB = FSGI::read ("./test.rgb");
+  imgB = FJPEG::read ("./test.jpg");
   printf ("Testing layer operations..\n");
   //normal (imgA, imgB, 0, 0, 0.5);
   //add    (imgA, imgB, 0, 0, 0.5);
@@ -21,10 +23,13 @@ void main ()
   //multiply    (imgA, imgB, 0, 0, 0.5);
   //divide    (imgA, imgB, 0, 0, 0.5);
   //screen    (imgA, imgB, 0, 0, 0.5);
-  overlay   (imgA, imgB, 0, 0, 1.0);
+  //overlay   (imgA, imgB, 0, 0, 1.0);
 
-  FSGI::write ("out.sgi", imgA, FSGI::RLE, 3);
-   
+  imgA = imgB->scale (100, 100);
+  imgA->channels (3);
+  //imgA->channels (4);
+  //FSGI::write ("image_scale.rgb", imgA, FSGI::RLE, 3);
+  FJPEG::write ("image_scale.jpg", imgA, 90);
   delete imgA;
   delete imgB;  
 }
