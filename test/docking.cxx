24f3bab5f40a37fb308fd9458e8817b9ff6ef6b7
brad <brad>
2000-05-17 05:49:26 +0000

    Make contents area obvious.

--- test/docking.cxx
+++ test/docking.cxx
@@ -1,9 +1,15 @@
+// dockable.cxx
+// Tests Fl_App_Window and Fl_Dockable_Window.
+
 #include <stdio.h>
 
 #include <FL/Fl.H>
 #include <FL/Fl_Box.H>
+
 #include <Flek/Fl_App_Window.H>
 #include <Flek/Fl_Dockable_Window.H>
+
+Fl_App_Window* application_window;
 
 void print_children(Fl_Group* o) {
   printf(">> label=\"%s\" xywh=%d,%d,%d,%d\n", o->label(), o->x(), o->y(), o->w(), o->h());
@@ -13,8 +19,6 @@ void print_children(Fl_Group* o) {
     printf ("   label=\"%s\" xywh=%d,%d,%d,%d\n", o->label(), o->x(), o->y(), o->w(), o->h());
   }
 }
-
-Fl_App_Window* application_window;
 
 Fl_Dockable_Window* new_dockable(const char* label, unsigned char grippertype) {
   Fl_Dockable_Window* dw = new Fl_Dockable_Window(0, 0, 200, 30, "Dockable Window");
@@ -36,26 +40,31 @@ void main() {
   // Make it resizable.
   application_window->resizable(application_window);
 
-  // Add a button to the application window.
-  // This actually adds the button to the "contents" child window.
-  Fl_Button* b = new Fl_Button(10, 10, 100, 30, "New Dockable");
+  // Make the contents area obvious.
+  application_window->contents()->box(FL_DOWN_BOX);
+
+  // Add some widgets.
+  // This actually adds the widgets to the "contents" child window.
+  Fl_Box* box = new Fl_Box(5, 5, Fl_Group::current()->w() - 5, 20, "Contents Window");
+  box->align(FL_ALIGN_INSIDE | FL_ALIGN_LEFT);
+  Fl_Button* b = new Fl_Button(10, 30, 100, 30, "New Dockable");
   b->callback(new_dockable);
 
   // We are done adding widgets to the application window.
   application_window->end();
 
+  // Create and dock some dockable windows.
   Fl_Dockable_Window* dw;
   dw = new_dockable("Dragable", FL_DRAGABLE);
   application_window->add_dockable(dw);
-
 #if 1
-  // Enable this section of code to demonstrate
+  // Disable this section of code if you don't want to demonstrate
   // the various gripper styles.
   dw = new_dockable("Shovable", FL_SHOVABLE);
   application_window->add_dockable(dw);
   dw = new_dockable("Drag/Shov", FL_DRAGABLE | FL_SHOVABLE);
   application_window->add_dockable(dw);
-  dw = new_dockable("Fixed", 0);
+  dw = new_dockable("No Gripper", 0);
   application_window->add_dockable(dw);
 #endif
 
@@ -98,6 +107,7 @@ void main() {
 
   // Print some diagnostics.
   print_children(application_window);
+  print_children(application_window->contents());
   print_children(application_window->tpack());
 
   Fl::run();  
